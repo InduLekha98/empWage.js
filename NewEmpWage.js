08ad011b9d5ab0279c4de4eba4e054eedcfa623f
InduLekha <rindulekha09.com>
2021-04-05 05:28:27 +0530

    Extend employee payroll class

--- NewEmpWage.js
+++ NewEmpWage.js
@@ -198,3 +198,40 @@ let nonWorkingDayStrArr = empObjArray
                                     .map(obj=>obj.dayNum);
 process.stdout.write("Logging Non Working Days:\n");
 console.log(nonWorkingDayStrArr);
+//UC12
+class EmployeePayrollData {
+//property
+id;
+salary;
+gender;
+startDate;
+
+//constructor
+constructor(...params) {
+ this.id = params[0];
+ this.name = params[1];
+ this.salary = params[2];
+ this.gender = params[3];
+ this.startDate = params[4];
+}
+//getter and setter method
+get name() { return this.name; }
+set name(name) { this._name = name; }
+// method
+toString() {
+  const options = { yesr: 'numeric',  month: 'long', day: 'numeric' };
+  const empDate = this.startDate === unDefined ? "unDefined" :
+                  this.startDate.toLocalDateString("en-US", options);
+  return "id=" + this.id + ",nmae='"+ this.name + ", salary=" +this.salary +", "+"gender=" + this.gender +", startDate=" +empDate;
+         "gender=" + this.gender + ", startDate=" +  empDate;
+ }
+}
+let employeePayrollData. = new EmployeePayrollData.(1, "Mark", 30000);
+console.log(employeePayrollData.toString());
+employeePayrollData..name = "john";
+console.log(employeePayrollData..toString());
+let newEmployeePayrollData. = new EmployeePayrollData(1, "Terisa", 30000, "F", new Date());
+console.log(newEmployeePayrollData.toString());
+
+
+
